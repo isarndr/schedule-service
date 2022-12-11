14d81ee72adea8af2860e901f2dfabaa4dd0fe0d
Isa Randra <isarandra@yahoo.com>
2022-12-11 18:31:03 +0700

    for swagger

--- src/main/java/com/codewithisa/scheduleservice/ScheduleServiceApplication.java
+++ src/main/java/com/codewithisa/scheduleservice/ScheduleServiceApplication.java
@@ -1,9 +1,17 @@
 package com.codewithisa.scheduleservice;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
+
+@OpenAPIDefinition(
+		servers = {
+				@Server(url = "/", description = "Default Server URL")
+		}
+)
 
 @SpringBootApplication
 public class ScheduleServiceApplication {
